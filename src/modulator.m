ed598717e5740faf71d7728f2bb08505817bc31a
cristmelo <cristianomelo_88@hotmail.com>
2015-09-10 01:50:25 -0300

    ASK modulation
    ASK modulation implemented

---
+++ src/modulator.m
@@ -0,0 +1,16 @@
+% DIGITAL DATA, ANALOG SIGNALS
+
+x = [0 1 0 0 1 1 0 0 ] % Book's example
+
+function ans = ASK( signal_vector, A, f, t )
+    n = length( signal_vector )
+    for( k = 1:n )
+      if( signal_vector(k) == 0 ) ans(k) = 0 
+      else ans(k) = A * cos( 2 * pi * f * t )  
+      endif
+    endfor
+
+endfunction  
+
+ASK( x, 1, 1, 1 )
+
