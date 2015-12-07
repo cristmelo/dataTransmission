1047dde6398b76df12d9c311a02e1cf768d036aa
Cristiano Melo <cristianomelo_88@hotmail.com>
2015-12-07 17:04:12 -0300

    FSK function OK

--- src/modulator.m
+++ src/modulator.m
@@ -14,3 +14,29 @@ endfunction
 
 ASK( x, 1, 1, 1 )
 
+
+function ans = FSK( signal_vector, A, fportadora, f_desvio )
+    n = length( signal_vector )
+    m = 100 * n
+    f1 = fportadora + f_desvio 
+    f2 = fportadora - f_desvio
+    t = [0:pi/400:2*pi];
+    
+    for( i = 1:800 )
+        if( signal_vector(ceil(i/100)) == 0 )
+          ans(i) = A * cos( 2 * pi * 1 * t(i) );
+        else
+          ans(i) = A * cos( 2 * pi * 2 * t(i) );  
+        endif
+    endfor
+    
+    subplot(2,1,1);
+    plot(ans);
+    grid on;
+    subplot(2,1,2);
+    stairs(signal_vector);
+    grid on;
+
+endfunction  
+
+FSK( x, 1, 2500, 500 )
